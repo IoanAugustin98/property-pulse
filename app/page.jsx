7a3f8b8842de898662bd90a736ffd665ed3dbac0
Test <test@SCTI-MacBook-Pro.local>
2024-04-07 21:14:15 +0300

    Same pages and basig app router

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export const metadata = {
     title: 'HomePage | PropertyPulse'
 };
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+        <h1 className="text-3xl">Welcome</h1>
+        <Link href="\properties">Show properties</Link>
+    </div>
   )
 }
 
