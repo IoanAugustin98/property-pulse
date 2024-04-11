9d3edd74c979a3fa4966d9276c08b6382d73a8bf
Test <test@SCTI-MacBook-Pro.local>
2024-04-11 20:25:00 +0300

    HomePage Components-Hero Section+Boxes+Footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import '@/assets/styles/global.css';
+import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 
 export const metadata = {
@@ -13,6 +14,7 @@ const MainLayout = ({ children }) => {
         <body>
             <Navbar />
             <mail>{ children }</mail>
+            <Footer />
         </body>
     </html>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 export const metadata = {
     title: 'HomePage | PropertyPulse'
@@ -6,10 +7,10 @@ export const metadata = {
 
 const HomePage = () => {
   return (
-    <div>
-        <h1 className="text-3xl">Welcome</h1>
-        <Link href="\properties">Show properties</Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
   )
 }
 
